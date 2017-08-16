32730f0f7cd29558e1c548538842ea851a0c4974
Jung <Jung@DaJung>
2017-08-17 02:11:24 +0900

    store 추가작성

--- src/store/main.java
+++ src/store/main.java
@@ -7,9 +7,17 @@ public class main {
 
 	public static void main(String[] args) {
 		String name = "\0";
+		int empl_num = 0;
+		int d_day = 0;
+		int income = 0;
+		int spend = 0;
+		
 		Scanner s = new Scanner(System.in);
 		System.out.print("가게이름을 입력하세요 : ");
 		name = s.nextLine();
+		System.out.print("고용할 종업원 수를 입력하세요 : ");
+		empl_num = s.nextInt();
+		System.out.println("가게가 생성되었습니다~\n");
 		
 
 		Random random = new Random();
@@ -21,7 +29,23 @@ public class main {
 		month = 1 + random.nextInt(12);
 		day = 1 + random.nextInt(30);
 		System.out.println("오늘은 " + year + "년 " + month + "월 " + day + "일 ♡");
+		printStore(name, empl_num, income, spend);
+		
+		
 
 	}
+	
+	static void printStore (String name, int num, int income, int spend) {
+		System.out.println("========가게정보========");
+		System.out.println("가게명 : " + name);
+		System.out.println("종업원 수 : " + num);
+		System.out.println("수입 : " + income);
+		System.out.println("지출 : " + spend);
+		System.out.println("======================");
+	}
+	
+	
+	
+	
 
 }

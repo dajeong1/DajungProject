e958bab3cf9b37ea14c694dcb24989da1133192d
Dj <Dj@DESKTOP-QSM7CFJ>
2017-09-07 21:08:37 +0900

    자바과제 1장만

---
+++ src/Java/First/Memojang.java
@@ -0,0 +1,17 @@
+package Java.First;
+import java.util.Scanner;
+
+public class Memojang {
+	public static void main(String[] args) {
+		int price, change, coin100, coin10;
+		Scanner s = new Scanner(System.in);
+		price = s.nextInt();
+		s.close();
+		change = 1000 - price;
+		coin100 = change / 100;
+		coin10 = (change % 100) / 10;
+		System.out.println("100원 동전 개수 = " + coin100);
+		System.out.println("10원 동전 개수 = " + coin10);
+		
+	}
+}
--- src/store/main.java
+++ src/store/main.java
@@ -15,7 +15,6 @@ public class main {
 	static int month = 0;
 	static int day = 0;
 	
-
 	public static void main(String[] args) {
 		
 		

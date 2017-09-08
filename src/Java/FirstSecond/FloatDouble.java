84f6c6a64c70a6be865d6b732773a39f9f900f2a
Jung <Jung@DaJung>
2017-09-09 01:09:49 +0900

    2장 1번과제

--- src/Java/FirstSecond/FloatDouble.java
+++ src/Java/FirstSecond/FloatDouble.java
@@ -1,5 +1,30 @@
 package Java.FirstSecond;
 
 public class FloatDouble {
-
+	public static void main(String[] args) {
+		float a, b, c, d, e;
+		double g, h, i, j, k;
+		
+		a = 'A';
+		b = 15;
+		//c = 1.5;  에러
+		d = 1.5f;
+		//e = 1.5d;  에러
+		
+		g = 'A';
+		h = 15;
+		i = 1.5;
+		j = 1.5f;
+		k = 1.5d;
+		
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("d = "+d+"\n");
+		System.out.println("g = "+g);
+		System.out.println("h = "+h);
+		System.out.println("i = "+i);
+		System.out.println("j = "+j);
+		System.out.println("k = "+k);
+		
+	}
 }

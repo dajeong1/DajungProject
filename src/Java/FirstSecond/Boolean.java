ae14a84d9252bc9a361f378c1e4c4be124320e77
Jung <Jung@DaJung>
2017-09-10 01:45:34 +0900

    과제 2장 2번해결

---
+++ src/Java/FirstSecond/Boolean.java
@@ -0,0 +1,12 @@
+package Java.FirstSecond;
+
+public class Boolean {
+	public static void main(String[] args) {
+		boolean a, b;
+		
+		a = true;
+		b = false;
+		System.out.println("a = " + a + "\n" + "b = " + b);
+		System.out.println(" 1 > 2 = " + (1 > 2));
+	}
+}
---
+++ src/Java/FirstSecond/Character.java
@@ -0,0 +1,18 @@
+package Java.FirstSecond;
+
+public class Character {
+
+	public static void main(String[] args) {
+		char ch1, ch2, ch3, ch4;
+		ch1 = 'A';
+		ch2 = 66;
+		ch3 = '\103'; //'C'에 대한 8진수 코드
+		ch4 = (char)(ch3 + 1);
+		System.out.print(ch1);
+		System.out.print(ch2);
+		System.out.print(ch3);
+		System.out.print(ch4);
+
+	}
+
+}

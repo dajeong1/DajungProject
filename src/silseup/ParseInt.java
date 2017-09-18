b34b5172ecf431fddc3514bf0d4294cfc439dda2
Dj <Dj@DESKTOP-QSM7CFJ>
2017-09-18 20:18:16 +0900

    dfafe

---
+++ src/silseup/ParseInt.java
@@ -0,0 +1,16 @@
+package silseup;
+
+public class ParseInt {
+
+	public static void main(String[] args) {
+		String a="100";
+		String b="200";
+		
+		int a1=Integer.parseInt(a);
+		int b1=Integer.parseInt(b);
+		
+		System.out.println(a+b);
+		System.out.println(a1+b1);
+	}
+
+}

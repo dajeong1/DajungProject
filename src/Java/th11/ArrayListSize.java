de1162c55ce385475fb8a37353e2834a895bf165
Jung <Jung@DaJung>
2017-12-04 00:32:33 +0900

    ㅇ

--- src/Java/th11/ArrayListSize.java
+++ src/Java/th11/ArrayListSize.java
@@ -5,13 +5,14 @@ public class ArrayListSize {
 	public static void main(String[] args) {
 		double sum = 0.0;
 		double avg = 0.0;
-		ArrayList<Double> list = new ArrayList<Double>();//생성자의 매개변수 = ArrayList의 size
+		ArrayList<Double> list = new ArrayList<Double>();
 		list.add(new Double(89.5));
 		list.add(new Double(62.0));
 		list.add(new Double(96.1));
 		list.add(new Double(75.4));
 		list.add(new Double(84.0));
 
+		//첫번째 방법
 		for(int i = 0; i<list.size(); i++){
 			sum += list.get(i);//cast 필요x
 		}
@@ -19,9 +20,9 @@ public class ArrayListSize {
 		
 		
 		sum = 0;
-		
+		//두번째 방법
 		int count = 0;
-		Iterator<Double> it = list.iterator();
+		Iterator<Double> it = list.iterator();//타입 파라미터 한쪽만 적기
 		while(it.hasNext()){
 			sum += it.next();
 			count++;

58c2c9dd957ae434981a40e6e724600e3c2436a5
Jung <Jung@DaJung>
2017-12-08 22:52:59 +0900

    g

---
+++ src/Java/th1314/ConParametor.java
@@ -0,0 +1,12 @@
+package Java.th1314;
+import java.io.*;
+
+public class ConParametor {
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/Java/th1314/DownOrder.java
+++ src/Java/th1314/DownOrder.java
@@ -6,17 +6,18 @@ public class DownOrder {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new FileReader(args[0]));
 		int[] a = new int[10];//파일 안 정수 10개 이하
-		int x;
+		String s;
+		int count = 0;
 		
 		for(int i = 0; i < 10; i++){
-			if ((x = br.read()) == -1) break;
-			if(x != ' ')
-				a[i] = x - '0';
+			if ((s = br.readLine()) == null) break;//★readLine()   String으로 값을 읽어냄
+			a[i] = Integer.parseInt(s);//String -> int(원시형) , 객체생성 필요x
+			count++;
 		}
 		
-		for (int i = 0; i < 10 - 1; i++)
-	    {
-	        for (int j = 0; j < 10 - 1 - i; j++)
+		for (int i = 0; i < count - 1; i++)
+		{
+	        for (int j = 0; j < count - 1 - i; j++)
 	        {
 	            if (a[j] < a[j + 1])
 	            {
@@ -28,8 +29,8 @@ public class DownOrder {
 	        }
 	    }
 		
-		System.out.println("내림차순 정렬 후 : ");
-		for (int i = 0; i < 10; i++){
+		System.out.println("내림차순 정렬 후2 : ");
+		for (int i = 0; i < count; i++){
 			System.out.print(a[i] + " ");
 		}
 		
